dd20c3bc58a802ffc29400798ebee3d308a8ce20
Anish <113669810+radlysamar@users.noreply.github.com>
2022-09-29 17:32:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+//chp - 1 
+//q1
+let a = "anish"
+let b = 6
+console.log(a + b)
+//q 2
+console.log(typeof (a + b))
+// q3
+const c = {
+  name: "anis",
+  class: 5,
+}
+// c = 12
+
+// q5
+c['friend'] = "viru"
+c['name'] = "ninu"
+console.log(c)
+
+
+// q5
+const pro = {
+
+  saponify: "to become converted into soap",
+  radiant: "bright with joy",
+  prevaricate: "to speak falsely or misleadingly; deliberately misstate or create an incorrect impression; lie.",
+
+
+  geitonogamy: "pollination of a flower by pollen from another flower on the same plant"
+
+}
+console.log(pro.geitonogamy)
+console.log(pro['prevaricate'])
